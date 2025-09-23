78adbbc5964134314ee26097d21ac0c905a0f065
Puneeth067 <kumarpuneeth631@gmail.com>
2025-09-23 22:15:58 +0530

    sjn

--- client/next.config.js
+++ client/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   /* config options here */
   // Ensure transpilation of workspace packages
   transpilePackages: ['@hirelens/shared-types', '@hirelens/config'],
+
 };
 
 export default nextConfig;
